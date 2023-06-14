cd17217727abb7db36074f3739ce92dcfc93b46d
Marek Bartoš <bartos.developer152@gmail.com>
2023-06-15 01:25:03 +0200

    Use maintained version of scss compiler

--- src/Filter/ScssFilter.php
+++ src/Filter/ScssFilter.php
@@ -2,37 +2,19 @@
 
 namespace WebLoader\Filter;
 
-use Leafo\ScssPhp\Compiler;
-use scssc;
+use ScssPhp\ScssPhp\Compiler;
 use function pathinfo;
 use const PATHINFO_DIRNAME;
 use const PATHINFO_EXTENSION;
 
-/**
- * Scss CSS filter
- */
 class ScssFilter
 {
 
-	/** @var Compiler */
-	private $sc;
+	private Compiler $sc;
 
 	public function __construct(?Compiler $sc = null)
 	{
-		$this->sc = $sc;
-	}
-
-	/**
-	 * @return Compiler|scssc
-	 */
-	private function getScssC()
-	{
-		// lazy loading
-		if (empty($this->sc)) {
-			$this->sc = new Compiler();
-		}
-
-		return $this->sc;
+		$this->sc = $sc ?? new Compiler();
 	}
 
 	/**
@@ -45,9 +27,9 @@ class ScssFilter
 	public function __invoke($code, \WebLoader\Compiler $loader, $file)
 	{
 		if (pathinfo($file, PATHINFO_EXTENSION) === 'scss') {
-			$this->getScssC()->setImportPaths(['', pathinfo($file, PATHINFO_DIRNAME) . '/']);
+			$this->sc->setImportPaths(['', pathinfo($file, PATHINFO_DIRNAME) . '/']);
 
-			return $this->getScssC()->compile($code);
+			return $this->sc->compileString($code)->getCss();
 		}
 
 		return $code;
